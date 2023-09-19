cf19d5f13564694a47762dcd18cbec9ad17d1c9e
unclebabykern <coren.frankel@gmail.com>
2023-09-19 15:29:27 -0600

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,65 @@
+
+/** Units/Terms */
+const Units: {} = {
+  // Generic
+  small: 'sm',
+  medium: 'md',
+  large: 'lg',
+  cup: 'c',
+  dozen: 'doz',
+  half: '½',
+  quarter: '¼',
+  bottle: 'btl',
+  drop: 'dp',
+  pinch: 'pn',
+  bunch: 'bn',
+  serving: 'serv',
+  each: 'ea',
+  
+  // Texture Specific
+  slice: 'sl',
+  // strip: 'str',
+  handful: 'hf',
+  // head: 'head',
+  piece: 'pc',
+  clove: 'cl',
+  // filet: 'fi',
+  sprig: 'sp',
+  // stalk: 'st',
+  // stick: 'st',
+  package: 'pkg',
+  // packet: 'pa',
+  // loaf: 'lf',
+  
+  // Height/Length/Depth
+  inch: 'in',
+  centimeter: 'cm',
+  millimeter: 'mm',
+  
+  // Weight
+  kilogram: 'kg',
+  gram: 'g',
+  milligram: 'mg',
+  microgram: 'µg',
+  pound: 'lb',
+  ounce: 'oz',
+  
+  // Volume/Capacity
+  gallon: 'gal',
+  quart: 'qt',
+  pint: 'pt',
+  'fluid ounce': 'fl oz',
+  liter: 'l',
+  centiliter: 'cl',
+  milliliter: 'ml',
+  
+  // Spoon Size
+  teaspoon: 'tsp',
+  tablespoon: 'tbsp',
+  dessertspoon: 'dsp'
+}
+
+/**
+ * @returns a string with a corresponding string abbreviation
+ */
+
