a34328e66e5714040e55732d72b6e42837dfb3c4
unclebabykern <coren.frankel@gmail.com>
2023-10-12 15:28:59 -0600

    add tests proving test incompetence

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -7,7 +7,14 @@ describe('index', () => {
   it('should return corresponding abbreviation', () => {
     expect(abbr('bacon').match('No abbreviation found for bacon'));
   });
-  it('should return corresponding abbreviation for multiple words', () => {
+  it('should return correct abbreviation individual strings', () => {
+    expect(abbr('bottle').match('btl'));
+    expect(abbr('bunch').match('pillow'));
+    expect(abbr('bushel').match('bl'));
+    expect(abbr('cup').match('c'));
+    expect(abbr('inches').match('in'));
+  });
+  it('should return corresponding abbreviation for multiple words in a string', () => {
     expect(abbr('fluid ounce').match('fl oz'));
     expect(abbr('half dozen').match('½ doz'));
   });
